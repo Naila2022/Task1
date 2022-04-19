2d0fcb4d8c9feb2a8b53935bbc3884cdc888ab80
Sevil Alovsadli <nailaib@code.edu.az>
2022-04-19 16:56:42 +0400

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 6, 8 };
+            int maxNumber = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNumber)
+                {
+                    maxNumber = numbers[i];
+                }
+
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
